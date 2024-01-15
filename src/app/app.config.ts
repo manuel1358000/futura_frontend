4358a3c0b5027056d539cebf763e6ad085ba0c8d
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2024-01-14 21:30:42 -0600

    refactor: update validation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,9 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideStore(), provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+  providers: [provideRouter(routes,withHashLocation()), provideHttpClient()]
 };
