e1d17231bf9b12af092f2f8a538bef612a36de84
M Fuentes <manuel13580@gmail.com>
2025-03-03 21:34:51 -0600

    Test

--- src/app/layout/components/nav-bar/components/radio-player/radio-player.component.ts
+++ src/app/layout/components/nav-bar/components/radio-player/radio-player.component.ts
@@ -13,7 +13,7 @@ export class RadioPlayerComponent {
 
   constructor() {
     this.sound = new Howl({
-      src: ['https://70a4-186-151-171-157.ngrok-free.app'], // Reemplaza con la URL de tu streaming de radio
+      src: ['https://1c375bcbfec8.ngrok.app/mystream.mp3'], // Reemplaza con la URL de tu streaming de radio
       format: ['mp3'], // El formato del streaming de radio
       html5: true, // Forzar el uso de HTML5 Audio
       autoplay: true, // Opcional: reproduce automáticamente al iniciar
--- src/app/pages/public/dashboard/home/home.component.ts
+++ src/app/pages/public/dashboard/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
 import { AnunciosResponse } from '../../../../models/anuncios/anunciosResponse.models';
 import { AnunciosService } from '../../../../service/anuncios/anuncios.service';
 import { SharedService } from '../../../../service/shared/shared.service';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +12,34 @@ import { SharedService } from '../../../../service/shared/shared.service';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(private sharedService: SharedService) {}
+  constructor(private sharedService: SharedService,private http: HttpClient) {}
+  @ViewChild('audioPlayer', { static: true }) audioPlayer!: ElementRef<HTMLAudioElement>;
+
+  private audioUrl = 'https://70a4-186-151-171-157.ngrok-free.app/mystream.mp3';
 
   ngOnInit() {
     this.sharedService.cargarAnuncios();
+    this.loadAudio();
+  }
+
+  private async loadAudio() {
+    try {
+      const response = await fetch(this.audioUrl, {
+        headers: {
+          'ngrok-skip-browser-warning': 'true'
+        }
+      });
+
+      if (!response.ok) throw new Error('Error al cargar el audio');
+
+      const blob = await response.blob();
+      const objectUrl = URL.createObjectURL(blob);
+
+      this.audioPlayer.nativeElement.src = objectUrl;
+      this.audioPlayer.nativeElement.play();
+    } catch (error) {
+      console.error('Error al reproducir el audio:', error);
+    }
   }
 
   obtenerAnuncioPorTag(tag: string): string | undefined {
